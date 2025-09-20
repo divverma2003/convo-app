1796f9d157ae6ab2c2d2ccc36ac7fceff2eb25f4
divverma2003 <divyamalikverma@gmail.com>
2025-09-20 11:47:19 -0400

    Initial Project Setup

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on http://localhost:${ENV.PORT}`);
+});
