f40d644f245109d7a5ed60ed34ff2f81918a2b1a
divverma2003 <divyamalikverma@gmail.com>
2025-09-21 10:30:04 -0400

    edited sentry integration

--- backend/instrument.mjs
+++ backend/instrument.mjs
@@ -1,14 +1,20 @@
 import * as Sentry from "@sentry/node";
 import { ENV } from "./src/config/env.js";
 
-Sentry.init({
-  dsn: ENV.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
-  environment: ENV.NODE_ENV || "development",
-  includeLocalVariables: true,
+if (!ENV.SENTRY_DSN) {
+  console.warn(
+    "[Sentry] SENTRY_DSN is not set. Skipping Sentry initialization."
+  );
+} else {
+  Sentry.init({
+    dsn: ENV.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+    environment: ENV.NODE_ENV || "development",
+    includeLocalVariables: true,
 
-  // Setting this option to true will send default PII (Personally Identifiable Information) such as
-  // user email addresses, IP addresses, etc. to Sentry. Be cautious when enabling this in production
-  sendDefaultPii: true,
-});
+    // Setting this option to true will send default PII (Personally Identifiable Information) such as
+    // user email addresses, IP addresses, etc. to Sentry. Be cautious when enabling this in production
+    sendDefaultPii: true,
+  });
+}
