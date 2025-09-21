cb2fa5030ee62f175525ac7623054f53f15f228e
divverma2003 <divyamalikverma@gmail.com>
2025-09-21 12:29:00 -0400

    Initial frontend setup + tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
-import React from "react";
+import { Routes, Route, Navigate } from "react-router";
 import {
   SignedIn,
   SignedOut,
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 
 const App = () => {
   return (
-    <header>
+    <div>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+          <SignInButton mode="modal" />
+        </Routes>
       </SignedOut>
       <SignedIn>
-        <UserButton />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </Routes>
       </SignedIn>
-    </header>
+    </div>
   );
 };
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserButton } from "@clerk/clerk-react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+};
+
+export default HomePage;
